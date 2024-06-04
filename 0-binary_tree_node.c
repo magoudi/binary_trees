539e10eb37da2c417b5e6cc16a18d97b9fd10d8e
magoudi <youssefmagedan@gmail.com>
2024-06-04 11:58:02 -0700

    f

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,6 +10,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode;
+
 	newnode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
 
 	if (newnode == NULL)
