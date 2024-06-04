525ca8519f10fbf98da96e16aea27f6db33ee803
magoudi <youssefmagedan@gmail.com>
2024-06-04 12:34:17 -0700

    f

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 
-/*binary_tree_insert_left - qisrgoiqrsgijasekf
+/*
+ * binary_tree_insert_left - qisrgoiqrsgijasekf
  * @parent: dsfjgiajesigasngdasbg
  * @value: nasefjasfojaefsi
  * Return: sudhgiohaesufhijgf
@@ -9,13 +10,14 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode;
+
 	newnode = malloc(sizeof(binary_tree_t));
 
 	if (newnode == NULL || parent == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	newnode->parent = parent;
 	newnode->n = value;
 	newnode->right = NULL;
