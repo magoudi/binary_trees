6dbabef01c997aef08bb4d81e3b05ff9554c7e77
magoudi <youssefmagedan@gmail.com>
2024-06-05 10:11:11 -0700

    f

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,27 +9,22 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *newnode;
 
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
-
-	new_node = binary_tree_node(parent, value);
-
-	if (new_node == NULL)
+	newnode = binary_tree_node(parent, value);
+	if (newnode == NULL)
 	{
 		return (NULL);
 	}
-
 	if (parent->left != NULL)
 	{
-		parent->left->parent = new_node;
-		new_node->left = parent->left;
+		parent->left->parent = newnode;
+		newnode->left = parent->left;
 	}
-
-	parent->left = new_node;
-
-	return (new_node);
+	parent->left = newnode;
+	return (newnode);
 }
