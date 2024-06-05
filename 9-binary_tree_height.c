663f11aa73a3e14de4ee435bda7530570b5cda0b
magoudi <youssefmagedan@gmail.com>
2024-06-05 22:36:58 +0300

    iii

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,16 +8,20 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
-    
-    left_height = 0;
-    right_height = 0;
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	size_t l = 0;
+	size_t r = 0;
 
-    return (left_height > right_height ? left_height : right_height) + 1;
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree)
+		{
+			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((l > r) ? l : r);
+	}
 }
