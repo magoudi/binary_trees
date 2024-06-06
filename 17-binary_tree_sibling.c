cb22aa5ed2917c89f60399ab21dfb931b7166c8f
magoudi <youssefmagedan@gmail.com>
2024-06-06 17:23:03 +0300

    f

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,6 +10,8 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	binary_tree_t *parent;
 
+	if (!node || !node->parent)
+		return (NULL);
 	parent = node->parent;
 	if (parent->left == node)
 		return (parent->right);
