a430f77755aa8fbdb67abbaa43278d02858e3cfa
magoudi <youssefmagedan@gmail.com>
2024-06-06 13:41:25 +0300

    f

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_depth - ieguhsgdfuojug
+ *@tree: tree
+ *Return: depth of tree
+*/
+
+size_t binary_tree_depth(const binary_tree_t* tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->parent == NULL)
+		return (0);
+	return (binary_tree_depth(tree->parent) + 1);
+}
