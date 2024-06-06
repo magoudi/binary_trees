73d12f6b71f00b7f43ef2ad682d52ca00f8523c7
magoudi <youssefmagedan@gmail.com>
2024-06-06 17:42:41 +0300

    f

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - ancestor
+ * @first: first
+ * @second: second
+ * Return: return
+*/
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,\
+const binary_tree_t *second)
+{
+	if (!first || !second || !first->parent || !second->parent)
+		return (NULL);
+	if (first->parent == second->parent)
+		return (first->parent);
+	else
+		return (binary_trees_ancestor(first->parent, second->parent));
+}
