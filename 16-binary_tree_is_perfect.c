fd8764737eefa7430ef41ead906245fe52698b64
magoudi <youssefmagedan@gmail.com>
2024-06-06 16:54:56 +0300

    f

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -83,6 +83,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (left_count != right_count)
 		return (0);
 	if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
-	return (1);
+		return (1);
 	return (1);
 }
