572d5ee7892c2ebe61a4b24d784d87b3858049bf
magoudi <youssefmagedan@gmail.com>
2024-06-06 17:20:58 +0300

    f

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds siblings
+ * @node: node
+ * Return: returns node
+*/
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *parent;
+
+	parent = node->parent;
+	if (parent->left == node)
+		return (parent->right);
+	else
+		return (parent->left);
+}
