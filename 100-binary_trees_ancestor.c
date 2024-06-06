9506875f10a1e50c2f12750dfaa1b82f6d20fe89
magoudi <youssefmagedan@gmail.com>
2024-06-06 17:45:50 +0300

    f

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,8 +7,8 @@
  * Return: return
 */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,\
-const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+									const binary_tree_t *second)
 {
 	if (!first || !second || !first->parent || !second->parent)
 		return (NULL);
