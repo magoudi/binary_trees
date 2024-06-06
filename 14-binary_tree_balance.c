c11e67c5f05b6308c07ab180831b3df63c87bd49
magoudi <youssefmagedan@gmail.com>
2024-06-06 15:22:20 +0300

    f

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - jkdfgjnalsefncadfnsglsa
+ * @tree: aslgknaslgknvaslgjdn
+ * Return: sjgnakngasgnsg
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
